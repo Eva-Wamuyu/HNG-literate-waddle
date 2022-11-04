7f0de3229ecf66f0b521516745f239a91e3aa3a8
Eva-Wamuyu <rutheve.eva@gmail.com>
2022-11-04 22:10:52 +0300

    read csv and output

--- app.py
+++ app.py
@@ -1,71 +1,68 @@
 import csv
 import hashlib
+import json
 
+csv_file = './assets/naming-all.csv'
+json_file = './assets/naming-all-teams.json'
+json_array = []
 
-csv_file = open('./assets/naming-all-teams.csv','r')
-json_file = open('./assets/naming-all-teams.json','rb')
+def create_json():
+    json_array = []
+    with open(csv_file,encoding='utf-8') as f:
+        f_reader = csv.DictReader(f)
+        for line in f_reader:
+            json_array.append(line)
+        
+    return json_array
 
-def get_sha_256():
-    
-    bytes = json_file.read()
-    sha_256 = hashlib.sha256(bytes).hexdigest()
-    
-    return sha_256
 
 def create_json_file():
-    for line in csv_file:
-        print(line)
-        try:
-            format = line['format']
-            name = line['name']
-            description = line['description']
-            series_number = line['series_number']
-            series_total = line['series_total']
-            attributes = line['attributes']
+    data = create_json()
+    with open(json_file,'w', encoding='utf-8') as f:
+        json_ = json.dumps(data,indent=10)
+        f.write(json_)
+    
 
-            for attribute in attributes:
-                trait_type = attribute[0]['trait_type']
-                value = attribute[0]['value']
-                trait_type= attribute[1]['trait_type']
-                value2 = attribute[1]['value']
-                trait_type2 = attribute[2]['trait_type']
-                value3 = attribute[2]['value']
-                min_value = attribute[3]['min_value']
-                max_value = attribute[3]['max_value']
 
-        
-        except KeyError:
 
-            obj = {}
 
+def get_sha_256():
+    with open(json_file,'rb') as f:
+        bytes = f.read()
+        sha_256 = hashlib.sha256(bytes).hexdigest()
+    return sha_256
 
 
 def append():
     sha_256 = get_sha_256()
     new_csv = []
-    csv_file_reader = csv.reader(csv_file,delimiter=',')
-    for line in csv_file_reader:
-            line.append(sha_256)
-            new_csv.append(line)
-    
+    csv_data = open(csv_file, 'r')
+    for line in csv.reader(csv_data,delimiter=','):
+        line.append(sha_256)
+        new_csv.append(line)
     return new_csv
+
+
 
 
 def add_to_output_file():
     updated_csv_content = append()
-    csv_file = open('./assets/naming-all-teams-output.csv','w')
+    csv_file = open('./assets/naming-all-output.csv','w')
     writer = csv.writer(csv_file)
     for line in updated_csv_content:
         writer.writerow(line)
-    csv_file.close()
-
+       
+    
+def handler():
+    create_json_file()
+    add_to_output_file()
+    
 
 
 if __name__ == '__main__':
     
-    add_to_output_file()
-    json_file.close()
-    csv_file.close()
+    handler()
+    
 
 
 
